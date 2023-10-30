3ba1edb1a2027c2c30b47de0254b96682709953a
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-10-30 15:48:44 -0700

    Starting my Hangman game

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string hiddenWord = "hangman";
+string userInput = "";
+
+// Startup
+Console.WriteLine("Welcome to hangman!\n\nPlease enter a word to begin:");
+userInput = Console.Read().ToString();
+
+Console.WriteLine($"Thank, you. Your word is: {userInput}");
